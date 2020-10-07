299920b272c281ef93508e32c85ee6a31930d372
FreSemin <59062254+FreSemin@users.noreply.github.com>
2020-10-07 17:34:48 +0300

    feat(slider-comp): init comp, create structure

--- YourBoots-Shop-App/src/app/app.module.ts
+++ YourBoots-Shop-App/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { HomeComponent } from './components/home/home.component';
 import { CartComponent } from './components/cart/cart.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
-    CartComponent
+    CartComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
