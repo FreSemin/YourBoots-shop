12d72c521e9e0db46a028bc3b9a3efd5eb8b7fea
FreSemin <59062254+FreSemin@users.noreply.github.com>
2020-10-07 10:45:28 +0300

    feat(App): init home, cart components, setting routers

--- YourBoots-Shop-App/src/app/app.module.ts
+++ YourBoots-Shop-App/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HomeComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
