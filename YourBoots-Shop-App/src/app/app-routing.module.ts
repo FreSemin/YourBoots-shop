42b6f7ab5f795a89aa7fe401188b1f2e9be381f1
FreSemin <59062254+FreSemin@users.noreply.github.com>
2020-11-06 01:31:53 +0300

    fix(all): fix ts lint warnings

--- YourBoots-Shop-App/src/app/app-routing.module.ts
+++ YourBoots-Shop-App/src/app/app-routing.module.ts
@@ -3,25 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 import { CartComponent } from './components/cart/cart.component';
 import { HomeComponent } from './components/home/home.component';
 
-
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'cart',
-    component: CartComponent
-  },
-  {
-    path: '**',
-    redirectTo: '',
-    pathMatch: 'full'
-  }
+	{
+		path: '',
+		component: HomeComponent
+	},
+	{
+		path: 'cart',
+		component: CartComponent
+	},
+	{
+		path: '**',
+		redirectTo: '',
+		pathMatch: 'full'
+	}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
