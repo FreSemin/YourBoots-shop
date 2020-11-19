5c5db5be7a6553fc0b1f4b1b01cd46dfcc082ea6
FreSemin <59062254+FreSemin@users.noreply.github.com>
2020-11-19 23:59:17 +0300

    Update(Backend): replace CORS to new file module

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,8 @@
 const express = require("express");
-
+const allowCors = require("./cors");
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Request-With, Content-Type, Accept"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT");
-  next();
-});
+app.use(allowCors);
 
 app.use("/api/catalog", (req, res, next) => {
   const catalog = {
