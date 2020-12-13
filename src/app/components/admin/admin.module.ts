a3b92d95af112009a09068e72a63703450a9c8dc
FreSemin <59062254+FreSemin@users.noreply.github.com>
2020-12-13 23:51:38 +0300

    fix(components): fix problem with multi import compoments be create ShareModule

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { SharedModule } from 'src/app/modules/shared/shared.module';
 
 @NgModule({
-	declarations: [AdminComponent],
+	declarations: [
+		AdminComponent
+	],
 	imports: [
 		CommonModule,
-		AdminRoutingModule
+		AdminRoutingModule,
+		SharedModule
 	]
 })
 export class AdminModule { }
