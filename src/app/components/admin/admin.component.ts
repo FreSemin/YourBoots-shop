cbfe44ed224ffcc2418e893b7dc2a94e2f067fa3
FreSemin <59062254+FreSemin@users.noreply.github.com>
2020-12-13 21:21:46 +0300

    feat(app): create admin module, component

---
+++ src/app/components/admin/admin.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss']
+})
+export class AdminComponent implements OnInit {
+
+  // tslint:disable-next-line: no-empty
+  constructor() { }
+
+  // tslint:disable-next-line: no-empty
+  public ngOnInit(): void {
+  }
+
+}
