89a97451326cc39b872931d99d3fb8377c0b2351
FreSemin <59062254+FreSemin@users.noreply.github.com>
2021-01-07 17:13:50 +0300

    fix(backend, userModel): required, module export

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,9 +4,9 @@ const uniqueValidator = require("mongoose-unique-validator");
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  access: { type: String, required: true, default: "user" },
+  access: { type: String, required: false, default: "user" },
 });
 
 userSchema.plugin(uniqueValidator);
 
-module.exports = mongoose.model("User", catalogElementSchema);
+module.exports = mongoose.model("User", userSchema);
