6871ab8079f42dea1dd02e0da5e6d4cdfb525025
FreSemin <59062254+FreSemin@users.noreply.github.com>
2021-01-07 17:17:13 +0300

    feat(backend, auth): admin signup request, hash password

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user");
 
 const router = express.Router();
 
 router.post("/admin/signup", (req, res, next) => {
-    
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hashPassword) => {
+      const admin = new User({
+        email: req.body.email,
+        password: hashPassword,
+        access: "admin",
+      });
+      admin.save().then((result) => {
+        res.status(201).json({
+          message: "Admin created",
+          result: result,
+        });
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
