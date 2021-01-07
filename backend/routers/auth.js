36ce947a517515f4e0a04326b4d1b4ddc3fdf4d1
FreSemin <59062254+FreSemin@users.noreply.github.com>
2021-01-07 18:38:29 +0300

    fix(backend, auth): catch block on admin signup

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -6,26 +6,26 @@ const User = require("../models/user");
 const router = express.Router();
 
 router.post("/admin/signup", (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hashPassword) => {
-      const admin = new User({
-        email: req.body.email,
-        password: hashPassword,
-        access: "admin",
-      });
-      admin.save().then((result) => {
+  bcrypt.hash(req.body.password, 10).then((hashPassword) => {
+    const admin = new User({
+      email: req.body.email,
+      password: hashPassword,
+      access: "admin",
+    });
+    admin
+      .save()
+      .then((result) => {
         res.status(201).json({
           message: "Admin created",
           result: result,
         });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: err,
+        });
       });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-      });
-    });
+  });
 });
 
 module.exports = router;
