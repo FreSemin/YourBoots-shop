553f256d372199502d4dbf343e80dbcdf47e2061
FreSemin <59062254+FreSemin@users.noreply.github.com>
2021-01-13 02:05:34 +0300

    feat(AuthService): save token to LS

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -11,6 +11,7 @@ export class AuthService {
 	private _token: string = '';
 	private _isAuthenticated: boolean = false;
 	private _tokenTimer: any;
+	private _toMilSec: number = 1000;
 
 	public adminEmail: string = '';
 
@@ -19,6 +20,53 @@ export class AuthService {
 		private _http: HttpClient,
 		private _router: Router,
 	) { }
+
+	private saveAuthDataLS(token: string, expirationDate: Date): void {
+		localStorage.setItem('token', token);
+		localStorage.setItem('expiration', expirationDate.toISOString());
+	}
+
+	private clearAuthDataLS(): void {
+		localStorage.removeItem('token');
+		localStorage.removeItem('expiration');
+	}
+
+	private getAuthDataLS(): any {
+		const tokenLS: string = localStorage.getItem('token');
+		const expirationDateLS: string = localStorage.getItem('expiration');
+
+		// tslint:disable-next-line: strict-boolean-expressions
+		if (!tokenLS || !expirationDateLS) {
+			return;
+		}
+		return {
+			token: tokenLS,
+			expirationDate: new Date(expirationDateLS),
+		};
+	}
+
+	private setAuthTimer(duration: number): void {
+		this._tokenTimer = setTimeout(() => {
+			this.onAdminLogout();
+		}, duration * this._toMilSec);
+	}
+
+	public autoAuthUser(): void {
+		const authData: { token: string, expirationDate: Date } = this.getAuthDataLS();
+
+		if (!authData) {
+			return;
+		}
+
+		const now: Date = new Date();
+		const expiresIn: number = authData.expirationDate.getTime() - now.getTime();
+
+		if (expiresIn > 0) {
+			this._token = authData.token;
+			this.setAuthTimer(expiresIn / this._toMilSec);
+			this._isAuthenticated = true;
+		}
+	}
 
 	public getToken(): string {
 		return this._token;
@@ -68,18 +116,20 @@ export class AuthService {
 		).subscribe((response: { token: string, expiresIn: number }) => {
 			const token: string = response.token;
 			const expiresInDuration: number = response.expiresIn;
-			const toMilSec: number = 1000;
 
 			this.adminEmail = form.value.adminName;
 
 			this._token = token;
 
 			if (token !== '') {
-				this._tokenTimer = setTimeout(() => {
-					this.onAdminLogout();
-				}, expiresInDuration * toMilSec);
+				this.setAuthTimer(expiresInDuration);
 
 				this._isAuthenticated = true;
+
+				const now: Date = new Date();
+				const expirationDate: Date = new Date(now.getTime() + expiresInDuration * this._toMilSec);
+				this.saveAuthDataLS(token, expirationDate);
+
 				form.reset();
 				this.redirectToAdmin();
 			}
@@ -88,6 +138,7 @@ export class AuthService {
 
 	public onAdminLogout(): void {
 		clearTimeout(this._tokenTimer);
+		this.clearAuthDataLS();
 		this._isAuthenticated = false;
 		this._token = '';
 		this.redirectToLogin();
