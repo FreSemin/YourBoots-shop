415e6f9c9608d06f278251642e93a2d46510693f
FreSemin <59062254+FreSemin@users.noreply.github.com>
2021-01-15 23:26:58 +0300

    chore(loginComp, authService): loginName to loginEmail

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -127,7 +127,7 @@ export class AuthService {
 		}
 
 		const adminAuthData: IAuthData = {
-			email: form.value.loginName,
+			email: form.value.loginEmail,
 			password: form.value.loginPassword,
 		};
 
@@ -140,7 +140,7 @@ export class AuthService {
 
 			this._userPermission = response.userPermission;
 
-			this.userEmail = form.value.loginName;
+			this.userEmail = form.value.loginEmail;
 
 			this._token = token;
 
