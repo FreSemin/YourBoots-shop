4980ab6907093ecb9976c1c4441e3da78b8676bb
FreSemin <59062254+FreSemin@users.noreply.github.com>
2021-02-07 16:38:36 +0300

    feat(CustomSnackBar_Comp): input data, up template

--- src/app/components/custom-snack-bar/custom-snack-bar.component.ts
+++ src/app/components/custom-snack-bar/custom-snack-bar.component.ts
@@ -1,7 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
-// import { AuthService } from 'src/app/services/auth/auth.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
 import { ModalService } from 'src/app/services/modal/modal.service';
+import { ISnackBarData } from '../models/snackBar/snack-bar-data.model';
 
 @Component({
 	selector: 'app-custom-snack-bar',
@@ -11,13 +12,21 @@ import { ModalService } from 'src/app/services/modal/modal.service';
 export class CustomSnackBarComponent implements OnInit {
 
 	constructor(
-		@Inject(MAT_SNACK_BAR_DATA) public data: string,
+		@Inject(MAT_SNACK_BAR_DATA) public data: ISnackBarData,
 		public modalService: ModalService,
-		// public authService: AuthService,
+		public authService: AuthService,
 	) { }
 
 	// tslint:disable-next-line: no-empty
 	public ngOnInit(): void {
+
+		if (this.data === null || this.data === undefined) {
+			this.data = {
+				text: '',
+				isLogin: false,
+			};
+		}
+
 	}
 
 }
