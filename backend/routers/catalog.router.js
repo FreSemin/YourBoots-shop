e9d26b592ae1e5846e97106d187a685e5b34dc63
FreSemin <59062254+FreSemin@users.noreply.github.com>
2021-02-15 00:10:51 +0300

    feat(Catalog, backend): multer file middleware

--- backend/routers/catalog.router.js
+++ backend/routers/catalog.router.js
@@ -1,37 +1,12 @@
 const express = require("express");
 
-const multer = require("multer");
-
 const router = express.Router();
 
 const checkAuth = require("../middleware/check-auth");
 const checkPermission = require("../middleware/check-permission");
+const extractFile = require("../middleware/multer-file");
 
 const CatalogController = require("../controllers/catalog");
-
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg",
-};
-
-const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValidType = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime type");
-
-    if (isValidType) {
-      error = null;
-    }
-
-    cb(error, "backend/images");
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(" ").join("-");
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + "." + ext);
-  },
-});
 
 router.get("", CatalogController.getCatalog);
 
@@ -39,7 +14,7 @@ router.post(
   "",
   checkAuth,
   checkPermission,
-  multer({ storage: fileStorage }).single("img"),
+  extractFile,
   CatalogController.createElement
 );
 
@@ -47,7 +22,7 @@ router.put(
   "/:id",
   checkAuth,
   checkPermission,
-  multer({ storage: fileStorage }).single("img"),
+  extractFile,
   CatalogController.updateElement
 );
 
