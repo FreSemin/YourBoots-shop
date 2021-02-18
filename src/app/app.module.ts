ac1cdb36e6a2cfc4ddebe19814c4728e323d8d3b
FreSemin <59062254+FreSemin@users.noreply.github.com>
2021-02-18 14:09:16 +0300

    chore(AppModule): delete unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { appReducers } from './store';
 import { EffectsModule } from '@ngrx/effects';
@@ -19,16 +18,13 @@ import { ContactModalEffects } from './store/effects/contact-modal.effects';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material-modules/meterial.module';
 import { MenuEffects } from './store/effects/menu.effects';
-import { HomeModule } from './components/home/home.module';
 import { ModalService } from './services/modal/modal.service';
 import { CardFormService } from './services/card-form/card-form.service';
 import { AuthGuard } from './guards/auth.guard';
 import { AuthService } from './services/auth/auth.service';
 import { AuthInterceptor } from './services/auth/auth.interceptor';
-import { AuthModule } from './components/auth/auth.module';
 import { AuthEffects } from './store/effects/auth.effects';
 import { ErrorInterceptor } from './error.interceptor';
-import { SnackBarModule } from './components/snack-bar/snack-bar.module';
 
 @NgModule({
 	declarations: [
@@ -38,13 +34,8 @@ import { SnackBarModule } from './components/snack-bar/snack-bar.module';
 		BrowserModule,
 		BrowserAnimationsModule,
 		AppRoutingModule,
-		FormsModule,
-		ReactiveFormsModule,
 		HttpClientModule,
 		MaterialModule,
-		HomeModule,
-		AuthModule,
-		SnackBarModule,
 		StoreModule.forRoot(appReducers),
 		EffectsModule.forRoot([
 			CatalogEffects,
